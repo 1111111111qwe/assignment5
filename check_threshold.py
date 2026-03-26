2a1952d1569a9cc32c2e7d021bdb819bd6a7aaec
Mohamed Ahmed <1111111111qwe.com>
2026-03-26 03:56:41 +0200

    work

--- check_threshold.py
+++ check_threshold.py
@@ -3,11 +3,11 @@ import mlflow
 import sys
 import os
 
-# tracking_uri = "file://" + os.path.abspath("mlruns")
-# mlflow.set_tracking_uri(tracking_uri)
-mlflow.set_tracking_uri("file:./mlruns")
+tracking_uri = "file://" + os.path.abspath("mlruns")
+mlflow.set_tracking_uri(tracking_uri)
+mlflow.set_experiment("assignment5")
 
-# print(f"Tracking URI: {tracking_uri}")
+print(f"Tracking URI: {tracking_uri}")
 
 with open("model_info.txt", "r") as f:
     run_id = f.read().strip()
@@ -15,8 +15,17 @@ with open("model_info.txt", "r") as f:
 print(f"Run ID: {run_id}")
 
 client = mlflow.tracking.MlflowClient()
-run = client.get_run(run_id)
-accuracy = run.data.metrics["accuracy"]
+
+runs = client.search_runs(
+    experiment_ids=["286619755676499285"],
+    filter_string=f"run_id = '{run_id}'"
+)
+
+if not runs:
+    print("Run not found in experiment!")
+    sys.exit(1)
+
+accuracy = runs[0].data.metrics["accuracy"]
 
 print(f"Accuracy: {accuracy}")
 
